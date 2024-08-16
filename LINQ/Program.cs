fcffc5de4feaada812f4b5431a90d1ae37792e35
iZahir.Developer <izahir.developer@gmail.com>
2024-08-16 23:40:08 +0530

    GroupJoin Method Syntax - Student

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -34,5 +34,56 @@ app.MapGet("/EmployeeSalary", () =>
 #endregion
 
 
+#region GroupJoin Method Syntax 
+
+app.MapGet("/Student", () =>
+{
+
+    // Student collection
+    IList<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John", Age = 18, StandardID = 1 } ,
+                new Student() { StudentID = 2, StudentName = "Steve",  Age = 21, StandardID = 1 } ,
+                new Student() { StudentID = 3, StudentName = "Bill",  Age = 18, StandardID = 2 } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20, StandardID = 2 } ,
+                new Student() { StudentID = 5, StudentName = "Ron" , Age = 21, StandardID = 3}
+            };
+
+    IList<Standard> standardList = new List<Standard>() {
+                new Standard(){ StandardID = 1, StandardName="Standard 1"},
+                new Standard(){ StandardID = 2, StandardName="Standard 2"},
+                new Standard(){ StandardID = 3, StandardName="Standard 3"}
+            };
+
+    
+
+    var groupJoin = standardList.GroupJoin(studentList,
+                    std => std.StandardID,
+                    s => s.StandardID,
+                    (std, studentGroup) => new
+                    {
+                        Students = studentGroup,
+                        StandFullName = std.StandardName
+                    });
+
+    StringBuilder str = new ();
+
+    foreach (var item in groupJoin)
+    {
+        str.Append(item.StandFullName);
+        str.AppendLine();
+
+        foreach (var stud in item.Students)
+        {
+            str.Append(stud.StudentName);
+            str.AppendLine();
+        }     
+    }
+
+    return str.ToString();
+
+
+});
+
+#endregion
 
 app.Run();
---
+++ LINQ/Student/Student.cs
@@ -0,0 +1,15 @@
+namespace LINQ;
+
+public class Student{
+
+	public int StudentID { get; set; }
+	public string StudentName { get; set; }
+	public int Age { get; set; }
+	public int StandardID { get; set; }
+}
+
+public class Standard{
+
+	public int StandardID { get; set; }
+	public string StandardName { get; set; }
+}
