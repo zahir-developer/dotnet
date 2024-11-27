141c34731fd5a2ba86ba6c2bf6801026de2c9fb7
iZahir <izahir.developer@gmail.com>
2024-11-27 22:28:30 +0530

    SOLID - OCP/SRP - App - Initial

---
+++ SOLID/Open_Closed_Priciple/Model/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Closed_Priciple.Model
+{
+    internal class PersonModel
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
---
+++ SOLID/Open_Closed_Priciple/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+    }
+}
