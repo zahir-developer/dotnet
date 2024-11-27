d4bd7df41a1c6baf2da73d67de64ff98cdd98441
iZahir <izahir.developer@gmail.com>
2024-11-27 22:10:39 +0530

    Abstract App changes

---
+++ AbstractApp/SqliteDataAccess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractApp
+{
+    public class SqliteDataAccess : DataAccess
+    {
+        public override string LoadConnectionString(string name)
+        {
+            string output = base.LoadConnectionString(name);
+            return output += "from SQLite";
+        }
+        public override void LoadData(string sql)
+        {
+            Console.WriteLine("Loading SQLLite Data");
+        }
+
+        public override void SaveData(string sql)
+        {
+            Console.WriteLine("Saving SQLLite Data");
+        }
+    }
+}
